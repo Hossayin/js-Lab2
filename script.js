e26e525a976c2a1d5fee860b89a0ce5b9e19dad1
Ali Hossein <alihossein96@gmail.com>
2019-10-23 09:04:27 -0400

    mastered

--- script.js
+++ script.js
@@ -19,16 +19,16 @@ function fight(player1, player2, player1Health, player2Health) {
   while (true) {
     let attacker = chooseOption(player1, player2);
     if (attacker === player1) {
-      let player2Health = attackPlayer(player2Health);
-      logHealth(player2, player2health);
+      player2Health = attackPlayer(player2Health);
+      logHealth(player2, player2Health);
 
       if (isDead(player2Health)) {
         logDeath(player1, player2);
         break;
       }
     } else {
-      let player1Health = attackPlayer(player1Health);
-      logHealth(player1, player1health);
+      player1Health = attackPlayer(player1Health);
+      logHealth(player1, player1Health);
       if (isDead(player1Health)) {
         logDeath(player2, player1);
         break;
